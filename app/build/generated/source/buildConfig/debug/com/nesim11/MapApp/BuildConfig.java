c65ef7e85fc97d8be474bd2a82edbd3b3c729560
Nesim Alma <48694570+Nesim11@users.noreply.github.com>
2021-06-19 17:34:27 +0300

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/nesim11/MapApp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.nesim11.MapApp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.nesim11.MapApp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
